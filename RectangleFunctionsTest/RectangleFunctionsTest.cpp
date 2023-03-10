eca83c652600544732e953dad92cad42baa2ecbf
Devon Rempel <DevonMrempel@outlook.com>
2023-03-10 18:11:09 -0500

    3 tests for the setWidth and 3 tests for the setLength functions added(lines 33-92).

--- RectangleFunctionsTest/RectangleFunctionsTest.cpp
+++ RectangleFunctionsTest/RectangleFunctionsTest.cpp
@@ -30,5 +30,65 @@ namespace RectangleFunctionsTest
 			result = getArea(lenPtr, widPtr);
 			Assert::AreEqual(8, result);
 		}
+		TEST_METHOD(SetLengthLowerLimitFunctionality)
+		{
+			int input = 0;
+			int resultLen = 2;
+
+			int* lenPtr = &resultLen;
+			setLength(input, lenPtr);
+			Assert::AreNotEqual(input, *lenPtr);
+		}//Will test the lower limit of included values. Using a value one lower then 1(lower limit)
+		//Since 0 is outside of accepted values, and AreNotEqual is used, test should pass
+		TEST_METHOD(SetLengthUpperLimitFunctionality)
+		{
+			int input = 100;
+			int resultLen = 2;
+			int* lenPtr = &resultLen;
+
+			setLength(input, lenPtr);
+			Assert::AreNotEqual(input, *lenPtr);
+		}//will test the upper limit of included values. Using a value one greater then 99(upper limit)
+		//Since 100 is outside of accepted values, and AreNotEqual is used, test should pass
+		TEST_METHOD(SetLengthEqualityFunctionality)
+		{
+			int input = 26;
+			int resultLen = 2;
+			int* lenPtr = &resultLen;
+
+			setLength(input, lenPtr);
+			Assert::AreEqual(input, *lenPtr);
+		}//Will test that the pointer is updated to the proper value using input value between 1 and 99(inclusive)
+		//Test Should pass with both *lenPtr and input being 26
+		TEST_METHOD(SetWidthUpperLimitFunctionality)
+		{
+			int input = 100;
+			int resultWid = 2;
+			int* WidPtr = &resultWid;
+
+			setWidth(input, WidPtr);
+			Assert::AreNotEqual(input, *WidPtr);
+		}//will test the upper limit of included values. Using a value one greater then 99(upper limit)
+		//Since 100 is outside of accepted values, and AreNotEqual is used, test should pass
+		TEST_METHOD(SetWidthLowerLimitFunctionality)
+		{
+			int input = 0;
+			int resultWid = 2;
+			int* WidPtr = &resultWid;
+
+			setWidth(input, WidPtr);
+			Assert::AreNotEqual(input, *WidPtr);
+		}//will test the lower limit of included values. Using a value one less then 1(lower limit)
+		//Since 0 is outside of accepted values, and AreNotEqual is used, test should pass
+		TEST_METHOD(SetWidthEqualityFunctionality)
+		{
+			int input = 25;
+			int resultWid = 2;
+			int* WidPtr = &resultWid;
+
+			setWidth(input, WidPtr);
+			Assert::AreEqual(input, *WidPtr);
+		}//Will test that the pointer is updated to the proper value using input value between 1 and 99(inclusive)
+		//Test Should pass with both *widPtr and input being 26
 	};
 }
